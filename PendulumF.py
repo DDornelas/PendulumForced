436f26e057c0e8c5743dd63c05c20b44f038a870
Daneil Dornelas <dornelasdaniel@id.uff.br>
2017-05-28 12:41:03 -0300

    Quadrado Da Diferença Espaço de Fases

--- PendulumF.py
+++ PendulumF.py
@@ -28,7 +28,7 @@ class Pendulo:
 		
 g=9.8
 gama=0.5
-A=0.01
+A=1.25
 wf=2/3.0
 dt=0.01
 t1=0
@@ -44,7 +44,8 @@ energy1=np.zeros(t.size)
 thet2=np.zeros(t.size)
 omega2=np.zeros(t.size)
 energy2=np.zeros(t.size)
-dif=np.zeros(t.size)
+dift=np.zeros(t.size)
+difw=np.zeros(t.size)
 
 print p1.theta
 
@@ -55,24 +56,30 @@ for i in range(t.size):
 	p2.theta=(p2.theta+np.pi)%(2*np.pi)-np.pi
 	thet1[i], omega1[i], energy1[i]=p1.theta, p1.w, p1.e
 	thet2[i], omega2[i], energy2[i]=p2.theta, p2.w, p2.e
-	dif[i]=(thet1[i]-thet2[i])**2
-
+	dift[i]=math.sqrt((thet1[i]-thet2[i])**2)
+	difw[i]=math.sqrt((omega1[i]-omega2[i])**2)
+	
 print p1.theta
 
-plt.figure(figsize=(16,5), dpi=96)
-plt.axis([0,140,-0.001,0.016])
+relax=4000
+tr,wr,t1r = thet1[relax:],omega1[relax:],t[relax:]
+difwr,diftr = difw[relax:],dift[relax:]
+
+plt.figure(figsize=(6,5), dpi=96)
 
 ax=plt.gca()
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
+ax.autoscale()
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel(r'Tempo(s)')
-plt.ylabel(r'Posi\c{c}\~{a}o($rad^{2}$)')
+plt.xlabel(r'Posi\c{c}\~{a}o($rad$)')
+plt.ylabel(r'Velocidade($\frac{rad}{s}$)')
 
-plt.title(r'Pendulum For\c{c}ado Diferen\c{c}a da Posi\c{c}\~{a}o Entre P\^{e}ndulos', fontsize=12)
+plt.title(r'Pendulum For\c{c}ado Espa\c{c}o de Fases da Diferen\c{c}a Entre P\^{e}ndulos', fontsize=12)
 plt.grid()
-plt.plot(t, dif, 'r-', label="A=0,01")
-plt.savefig("QD001.pdf", dpi=96)
+plt.plot(dift, difw, 'r-', label="A=1,25")
+plt.legend(loc='upper right')
+plt.savefig("EFQD125.pdf", dpi=96)
 plt.show()
