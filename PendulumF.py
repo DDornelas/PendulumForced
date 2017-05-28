4c4bb8707da40dead5cc0a57df51d64d8a01fcb6
Daneil Dornelas <dornelasdaniel@id.uff.br>
2017-05-28 11:16:43 -0300

    Quadrado Da Diferença

--- PendulumF.py
+++ PendulumF.py
@@ -28,47 +28,51 @@ class Pendulo:
 		
 g=9.8
 gama=0.5
-A=0.75
+A=0.01
 wf=2/3.0
 dt=0.01
 t1=0
 
 p1=Pendulo(1, 10, np.pi/6, 0)
+p2=Pendulo(1, 10, 0.4, 0)
 
 tmax=20*p1.T
 t=np.arange(0, tmax, dt)
-thet=np.zeros(t.size)
-omega=np.zeros(t.size)
-energy=np.zeros(t.size)
+thet1=np.zeros(t.size)
+omega1=np.zeros(t.size)
+energy1=np.zeros(t.size)
+thet2=np.zeros(t.size)
+omega2=np.zeros(t.size)
+energy2=np.zeros(t.size)
+dif=np.zeros(t.size)
 
 print p1.theta
 
 for i in range(t.size):
 	p1.move(t[i])
+	p2.move(t[i])
 	p1.theta=(p1.theta+np.pi)%(2*np.pi)-np.pi
-	thet[i], omega[i], energy[i]=p1.theta, p1.w, p1.e
+	p2.theta=(p2.theta+np.pi)%(2*np.pi)-np.pi
+	thet1[i], omega1[i], energy1[i]=p1.theta, p1.w, p1.e
+	thet2[i], omega2[i], energy2[i]=p2.theta, p2.w, p2.e
+	dif[i]=(thet1[i]-thet2[i])**2
 
 print p1.theta
 
-relax=4000
-plt.figure(figsize=(8,12),facecolor='white')
-tr,wr,t1r = thet[relax:],omega[relax:],t[relax:]
-
-plt.figure(figsize=(6,5), dpi=96)
+plt.figure(figsize=(16,5), dpi=96)
+plt.axis([0,140,-0.001,0.016])
 
 ax=plt.gca()
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-ax.autoscale()
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel(r'Posi\c{c}\~{a}o(s)')
-plt.ylabel(r'Velocidade($\frac{m}{s}$)')
+plt.xlabel(r'Tempo(s)')
+plt.ylabel(r'Posi\c{c}\~{a}o($rad^{2}$)')
 
-plt.title(r'Pendulum For\c{c}ado Espa\c{c}o de Fases ', fontsize=12)
+plt.title(r'Pendulum For\c{c}ado Diferen\c{c}a da Posi\c{c}\~{a}o Entre P\^{e}ndulos', fontsize=12)
 plt.grid()
-plt.scatter(tr, wr, s=0.00001, label="A=0,75")
-plt.legend(loc='upper right')
-plt.savefig("EF075R.pdf", dpi=96)
+plt.plot(t, dif, 'r-', label="A=0,01")
+plt.savefig("QD001.pdf", dpi=96)
 plt.show()
